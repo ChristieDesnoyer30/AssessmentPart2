a182fb914abf7dc18568ec40602fdd324d792230
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-02-06 09:36:46 -0500

    Adding properties and methods to Animal and its child class BabyAnimal

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,47 @@
 package com.company;
 
 public class Animal {
+
+    private String name;
+    private String species;
+    private String gender;
+
+    public Animal(String name, String species, String gender) {
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+    }
+
+    public void printAnimal(){
+
+        System.out.println("The " + species + "is named " + name + ", and is a " + gender);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+
+
 }
--- src/com/company/BabyAnimal.java
+++ src/com/company/BabyAnimal.java
@@ -1,4 +1,26 @@
 package com.company;
 
 public class BabyAnimal extends Animal {
+
+    private String birthDate;
+
+    public BabyAnimal(String name, String species, String gender, String birthDate) {
+        super(name, species, gender);
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public void printAnimal(){
+        System.out.println( "This is a baby " + getGender()+ " " + getSpecies() + ", named" + getName() + "."
+        + "They were born in the zoo on " + birthDate);
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+    
 }
